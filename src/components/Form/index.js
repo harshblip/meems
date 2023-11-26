36fd643eaead6f5724b533531159ec0a5747c45e
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2023-11-27 02:28:59 +0530

    work on meme call on api

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,9 +1,10 @@
 import { useState } from 'react';
+import axios from 'axios';
 import './index.css'
 
 export default function ControlledComponent() {
-  const [first, setFirst] = useState('');
-  const [second, setSecond] = useState('');
+  const [first, setFirst] = useState("");
+  const [second, setSecond] = useState("");
 
   const handleChangeF = (e) => {
     setFirst(e.target.value);
@@ -19,6 +20,24 @@ export default function ControlledComponent() {
     console.log(second);
   }
 
+  const [memeUrl, setMemeUrl] = useState('');
+
+  const fetchMeme = async () => {
+    const url = 'http://alpha-meme-maker.herokuapp.com/memes/:id';
+    const options = {
+      method: 'GET',
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result.data);
+      setMemeUrl(result.image);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <form onSubmit={handleSubmit}>
       <div className='flex flex-col font-notosans mb-4 mt-4'>
@@ -26,21 +45,24 @@ export default function ControlledComponent() {
           <input
             type="text"
             first={setFirst}
-            onChange={handleChangeF} 
-            placeholder = 'Shut up'
-            className = 'w-40 h-4 border p-4 text-xs rounded-lg f '
-            />
+            onChange={handleChangeF}
+            placeholder='Shut up'
+            className='w-40 h-4 border p-4 text-xs rounded-lg f '
+          />
           <input
             type="text"
             second={setSecond}
             onChange={handleChangeS}
-            placeholder = 'Take my money'
-            className = 'w-40 h-4 border p-4 text-xs rounded-lg f'
-            />
+            placeholder='Take my money'
+            className='w-40 h-4 border p-4 text-xs rounded-lg f'
+          />
         </label>
       </div>
+      <img src={memeUrl} alt="meme" width='50' className='rounded-lg' />
       <button
-        className='w-[90%] mr-4 ml-3 bg-white border hover:bg-slate-700  hover:border-white transition-all hover:text-white  border-black rounded-lg p-3 text-black'>
+        className='w-[90%] mr-4 ml-3 bg-white border hover:bg-slate-700  hover:border-white transition-all hover:text-white  border-black rounded-lg p-3 text-black'
+        onClick={fetchMeme}
+      >
         Get a new meem image 🎉
       </button>
     </form>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,12 +3,23 @@ import arpitB from '../../media/arpit-bala-celebration.gif';
 import './index.css'
 
 export default function Header() {
+
+  const consoleOutput = () => {
+    console.log("hi babe");
+  }
+
   return (
     <div className='header justify-center space-x-5 font-notosans flex rounded-xl  p-8'>
 
-       <img src = {arpitB} alt = 'feels' height='10' width='80' className='bg-transparent rounded-lg' />
+      <img
+        src={arpitB}
+        alt='feels'
+        width='80'
+        onMouseEnter={consoleOutput}
+        className='bg-transparent rounded-xl object-contain  hover:{consoleOutput} '
+      />
 
-       <h2 className='text-white text-2xl mt-4'> Meem Generator </h2>
+      <h2 className='text-white text-2xl mt-4'> Meem Generator </h2>
     </div>
   )
 }
